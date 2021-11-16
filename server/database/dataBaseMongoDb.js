817df21c94b4dae0e66853200fec895544c757c9
SAGITOR <montanius12@gmail.com>
2021-11-16 00:33:59 -0300

    models creados

--- server/database/dataBaseMongoDb.js
+++ server/database/dataBaseMongoDb.js
@@ -1,6 +1,5 @@
 const { MONGODB_URI } = require('../const/const.js');
 const mongoose = require('mongoose');
-const Usuario = require('../models/usuario');
 
 mongoose.connect( MONGODB_URI,{
     useNewUrlParser: true,
@@ -11,24 +10,6 @@ const connection = mongoose.connection;
 
 connection.once('open', () =>{
     console.log('MongoDB is connected');
-    b();
 });
-const b = async () => {
 
-    try{
-        const a = await new Usuario(
-            { 
-                rut: 123123124, 
-                nombres: 'asfasdasd', 
-                apellidos: 'asdasfasd', 
-                contraseña: 'fasdasdasd', 
-                email: '1243214123',
-                telefono: 412321, 
-                saldo: 312312});
-        await a.save();
-        console.log('aaaa');
-    }catch(error){
-        console.log(error);
-    }
-}
 
