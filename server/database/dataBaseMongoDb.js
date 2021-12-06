203cd1c917f71ee5e88723b65a3a48155a0c04ab
Soujiro17 <vicente_tomi@hotmail.com>
2021-12-05 23:11:58 -0300

    no se

--- server/database/dataBaseMongoDb.js
+++ server/database/dataBaseMongoDb.js
@@ -1,15 +1,15 @@
-const { MONGODB_URI } = require('../const/const.js');
-const mongoose = require('mongoose');
-const Usuario = require('../models/usuario');
+const { MONGODB_URI } = require("../const/const.js");
+const mongoose = require("mongoose");
 
-mongoose.connect( MONGODB_URI,{
+mongoose
+  .connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-} ).catch(error => console.log(error));
+  })
+  .catch((error) => console.log(error));
 
 const connection = mongoose.connection;
 
-connection.once('open', () =>{
-    console.log('MongoDB is connected');
+connection.once("open", () => {
+  console.log("MongoDB is connected");
 });
-
